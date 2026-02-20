150173abeab8deb38fef6315771db240d2a6a773
Vaibhav Lohar <loharvb@gmail.com>
2026-02-20 18:08:50 +0530

    Project created..

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TaskPage from './features/tasks/tasksPage'
+
+function App() {
+
+
+  return (
+    <>
+       <TaskPage></TaskPage>
+    </>
+  )
+}
+
+export default App
