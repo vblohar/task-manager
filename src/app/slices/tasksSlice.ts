0d7a9e897a52fc7a35c260556703cdec66dff478
Vaibhav Lohar <loharvb@gmail.com>
2026-02-24 13:15:34 +0530

    Add task model and implement task management features

--- src/app/slices/tasksSlice.ts
+++ src/app/slices/tasksSlice.ts
@@ -1,13 +1,36 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import type { Task } from "../models/task";
+import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { Task } from "../models/types";
+
+
 
 interface TasksState {
     tasks: Task[];
+    loading: boolean;
+    error: string | null;
 }
+
+const dummyTask: Task[] =[
+    {id: "1", title: "Vaibhav", completed: false },
+    {id: "2", title: "Sarang", completed: false }
+]
 
 const initialState: TasksState = {
     tasks: [],
+    loading: false,
+    error: null
 }
+
+export const fetchTasks = createAsyncThunk(
+    "tasks/fetchTasks",
+    async () => {
+        const response = await new Promise<any>((resolve) => 
+        setTimeout(() => resolve(
+            dummyTask
+        ), 1000));
+        console.log(response);
+        return response;
+    }
+)
 
 const tasksSlice = createSlice({
     name: "tasks",
@@ -16,8 +39,40 @@ const tasksSlice = createSlice({
         addTask: (state, action: PayloadAction<Task>) => {
             state.tasks.push(action.payload);
         },
+
+        deleteTask: (state, action: PayloadAction<string>) => {
+            state.tasks = state.tasks.filter(task => 
+                task.id !== action.payload
+            );
+        },
+
+        toggleTask: (state, action: PayloadAction<string>) => {
+            const task = state.tasks.find(task => task.id === action.payload);
+            if(task) task.completed === !task.completed;
+        },
+
+        upateTask: (state, action: PayloadAction<{id: string; title: string}>) => {
+            const task = state.tasks.find(task => 
+                task.id === action.payload.id
+            );
+            if(task) task.title = action.payload.title;
+        }
+    },
+    extraReducers(builder) {
+        builder
+        .addCase(fetchTasks.pending, (state) => {
+            state.loading = true;
+        })
+        .addCase(fetchTasks.fulfilled, (state, action) => {
+            state.loading = false;
+            state.tasks = action.payload;
+        })
+        .addCase(fetchTasks.rejected, (state) => {
+            state.loading = false;
+            state.error = "Failed to fetch tasks";
+        })
     },
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, deleteTask, upateTask, toggleTask } = tasksSlice.actions;
 export default tasksSlice.reducer;
--- src/features/tasks/tasksPage.tsx
+++ src/features/tasks/tasksPage.tsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
-import { addTask } from "../../app/slices/tasksSlice";
+import { addTask, deleteTask, fetchTasks, toggleTask, upateTask } from "../../app/slices/tasksSlice";
 
 export default function TaskPage() {
     const [title, setTitle] = useState("");
+    const [editId, setEditId] = useState<string | null>(null);
+    var items;
+   
     const dispatch = useAppDispatch();
     const tasks = useAppSelector((state) => state.tasks.tasks);
 
-    const handleAdd =() => {
-        dispatch(
+    const handleSubmit = () => {
+        if (editId) {
+            dispatch(
+                upateTask({
+                    id: editId,
+                    title,
+                })
+            );
+            
+            setEditId(null);
+        } else if(title.length > 0) dispatch(
             addTask({
-                id: Date.now.toString(),
+                id: Date.now().toString(),
                 title,
                 completed: false,
             })
@@ -18,21 +30,47 @@ export default function TaskPage() {
         setTitle("");
     }
 
+    useEffect(() =>{
+       dispatch(fetchTasks());
+    },[])
+
     return (
-        <div className="p-5">
-            <h1>Task Manager</h1>
+        <div className="p-5 flex flex-col items-center">
+            <h1 className="text-3xl font-bold p-6">Task Manager</h1>
 
             <input className="border p-2 mr-2"
-            value={title} 
-            onChange={(e) => setTitle(e.target.value)}
-            type="text" />
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                type="text" />
 
-            <button className="bg-blue-500 text-white px-8" onClick={() => handleAdd()}>Add</button>
+            <button className="bg-blue-500 text-white px-8 mt-4" onClick={() => handleSubmit()}>{editId ? "Save" : "Add"}</button>
 
             <ul className="mt-4">
                 {
-                    tasks.map((task: any) => (
-                        <li key={task.id}>{task.title}</li>
+                    tasks.map((task) => (
+                        <li key={task.id} className="mb-2 flex justify-between">
+                            <div>
+                            <span className={`mr-8 ${task.completed ? "line-through" : ""
+                                }`}
+                            >
+                                {task.title}
+                            </span>
+                            </div>
+                            <div className="flex justify-end">
+                            <button onClick={() => dispatch(toggleTask(task.id))}>
+                                ✔
+                            </button>
+                            <button className="ml-2" onClick={() => {
+                                setEditId(task.id);
+                                setTitle(task.title);
+                            }}>
+                                ✏
+                            </button>
+                            <button className="ml-2 text-red-500" onClick={() => dispatch(deleteTask(task.id))}>
+                                ❌
+                            </button>
+                            </div>
+                        </li>
                     ))
                 }
             </ul>
